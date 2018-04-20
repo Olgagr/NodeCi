5d8af90171fe697726302e95ff96e13607d73d9c
Olga Grabek <olga.grabek@gmail.com>
2018-04-20 07:37:37 +0200

    Use yarn not npm client, first redis caching implementation

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
 
-const Blog = mongoose.model('Blog');
+const Blog = mongoose.model("Blog");
 
 module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+  app.get("/api/blogs/:id", requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
       _id: req.params.id
@@ -13,13 +13,30 @@ module.exports = app => {
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+  app.get("/api/blogs", requireLogin, async (req, res) => {
+    const redis = require("redis");
+    const redisUrl = "redis://127.0.0.1:6379";
+    const client = redis.createClient(redisUrl);
+    const util = require("util");
+    client.get = util.promisify(client.get);
 
+    // do we have any cached data related to this query
+    const cachedBlogs = await client.get(req.user.id);
+
+    // if yes, then respond and returm
+    if (cachedBlogs) {
+      console.log("serving from cache");
+      return res.send(JSON.parse(cachedBlogs));
+    }
+
+    // if no, go to mongoo and cache it for later
+    const blogs = await Blog.find({ _user: req.user.id });
+    console.log("serving from mongo");
     res.send(blogs);
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post("/api/blogs", requireLogin, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
