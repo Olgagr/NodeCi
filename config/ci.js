25e348c2e76c856cea0b6f8b9e0fdf4eb1f88df9
Olga Grabek <olga.grabek@gmail.com>
2018-05-12 11:17:05 +0200

    Setup travis

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     "555412386375-tt0q79bpbvsf6i9fjvsk6kasvldlmk9r.apps.googleusercontent.com",
   googleClientSecret: "6rbIU5j5hFMd-P5LDVhHejpa",
-  mongoURI: "mongodb://root:abcd1234@ds131997.mlab.com:31997/blog_everyone",
+  mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
   cookieKey: "123123123",
   redisUrl: "redis://127.0.0.1:6379"
 };
